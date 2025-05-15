1a7c2e59de48117a1c54dc133267a1b47fce2afb
adithya-naik <jatothadithyanaik@gmail.com>
2025-05-15 19:06:28 +0530

    Unknown, any & never return type

---
+++ l5/app.ts
@@ -0,0 +1,24 @@
+// Unknown, any & never return type
+
+// i want the clear difernwce between them
+
+
+
+console.log("Hii")
+
+let userInput : unknown;
+// let input : any;
+let userName : string;
+
+// userName = userInput
+// the above throws the error to fix that we need to first check the type of userInput then if it matches with the type of username then we can assign it ... till then it  shows the error
+
+
+
+
+// never return type ;
+// this is assigned to the function thta doesnt return anything like wehn a new eeroor id thrown and the code under it returns nothing so it can be asssoigned with the never data return type;
+
+
+
+ 
